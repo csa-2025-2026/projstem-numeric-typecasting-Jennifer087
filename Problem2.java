952f1534cae1130670a625eea91432fdc3ef1451
Jennifer087 <jenniferhuynh087@gmail.com>
2025-09-11 17:35:49 +0000

    did most of the assignment

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,22 @@
+public class Problem2
+{
 
+
+public static void main (String [] args)
+{
+// For positive numbers
+// (int) (x+0.5);
+
+// For negative numbers
+// (int)(x-0.5);
+
+double x = 57.3934;
+double y = 22.5211;
+int z = x + y;
+System.out.println (z);
+
+
+
+}
+
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,18 @@
+public class Problem3
+{
+public static void main (String [] args)
+{
+ double num = 867.5309;
+ int truncNum = (int) (num * 1000); //867530
+ System.out.println (truncNum % 10); //printing the last digit; 0
+ truncNum = truncNum / 10;
 
+System.out.println(truncNum % 10); //3
+truncNum = truncNum / 10 // 8675
+
+System.out.println(trunc % 10) //5
+truncNum = truncNum /10; // 867
+
+
+}
+}
